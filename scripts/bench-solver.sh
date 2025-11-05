4bb8e414dc6c7c5816ca1e3240bd393f78e1ec39
KonaeAkira <longtruong2411@gmail.com>
2025-11-05 23:00:37 +0100

    Re-add disabled config

--- scripts/bench-solver.sh
+++ scripts/bench-solver.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 cmds=(
     "solve -r 35829 -s 4900 4800 620 --manipulation" # Rarefied Tacos de Carne Asada
     "solve -r 35829 -s 4900 4800 620 --manipulation --adversarial" # Rarefied Tacos de Carne Asada
-    # "solve -r 35830 -s 5428 5333 737 --manipulation --heart-and-soul" # Archeo Kingdom Broadsword
+    "solve -r 35830 -s 5428 5333 737 --manipulation --heart-and-soul" # Archeo Kingdom Broadsword
 )
 
 echo "| Command | Time | Memory |"
