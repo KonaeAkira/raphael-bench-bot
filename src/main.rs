7ca1829f8d9d18bc68f6af51e85e4e8f2a9d5295
KonaeAkira <longtruong2411@gmail.com>
2025-11-05 22:12:44 +0100

    Add benchmarking

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Write;
+use std::process::Command;
 use std::sync::Arc;
 
 use axum::response::IntoResponse;
@@ -18,20 +20,85 @@ struct Comment {
 }
 
 #[derive(Debug, Deserialize)]
+struct Repository {
+    default_branch: String,
+}
+
+#[derive(Debug, Deserialize)]
 struct Payload {
     action: String,
     issue: Issue,
     comment: Comment,
+    repository: Repository,
+}
+
+fn checkout_and_build_raphael_cli_on_branch(branch_name: &str) {
+    let raphael_dir = std::env::var("RAPHAEL_DIR").expect("missing RAPHAEL_DIR var");
+    Command::new(format!("git checkout {branch_name}"))
+        .current_dir(&raphael_dir)
+        .status()
+        .unwrap();
+    Command::new(format!("cargo install --path raphael-cli"))
+        .current_dir(&raphael_dir)
+        .status()
+        .unwrap();
+}
+
+fn checkout_and_build_raphael_cli_on_pr(pr_number: u32) {
+    let raphael_dir = std::env::var("RAPHAEL_DIR").expect("missing RAPHAEL_DIR var");
+    Command::new(format!("gh pr checkout {pr_number}"))
+        .current_dir(&raphael_dir)
+        .status()
+        .unwrap();
+    Command::new(format!("cargo install --path raphael-cli"))
+        .current_dir(&raphael_dir)
+        .status()
+        .unwrap();
+}
+
+fn run_benchmark() -> String {
+    let scripts_dir = "./scripts";
+    let output = Command::new("bench-solver.sh")
+        .current_dir(scripts_dir)
+        .output()
+        .unwrap();
+    String::from_utf8(output.stdout).unwrap()
 }
 
 async fn webhook_handler(GithubEvent(payload): GithubEvent<Payload>) -> impl IntoResponse {
     dbg!(&payload);
+    if payload.action == "created"
+        && payload.comment.author_association == "OWNER"
+        && payload.comment.body.trim() == "/bench"
+    {
+        let mut message = String::new();
+
+        checkout_and_build_raphael_cli_on_branch(&payload.repository.default_branch);
+        writeln!(
+            message,
+            "Benchmarking `{}`:\n{}\n\n",
+            payload.repository.default_branch,
+            run_benchmark()
+        )
+        .unwrap();
+
+        checkout_and_build_raphael_cli_on_pr(payload.issue.number);
+        writeln!(
+            message,
+            "Benchmarking `pr {}`:\n{}\n\n",
+            payload.issue.number,
+            run_benchmark()
+        )
+        .unwrap();
+
+        dbg!(&message);
+    }
     StatusCode::OK
 }
 
 #[tokio::main]
 async fn main() {
-    let secret = std::env::var("GITHUB_WEBHOOK_SECRET").unwrap();
+    let secret = std::env::var("GITHUB_WEBHOOK_SECRET").expect("missing GITHUB_WEBHOOK_SECRET var");
     let app = Router::new()
         .route("/webhooks", post(webhook_handler))
         .with_state(GithubToken(Arc::new(secret)));
